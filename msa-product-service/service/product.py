351bb0d72a1de183f399e01f4a23ccc7e639fa15
sun12wook11 <sun12wook11@gmail.com>
2024-10-15 15:32:34 +0900

    상품관리처리

---
+++ msa-product-service/service/product.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+
+from models.product import Product
+from schema.product import ProductBase
+
+# 회원가입 처리
+# 기본 회원정보 + 번호,가입일
+def register(db: Session, product: ProductBase):
+    product = Product(**product.model_dump())
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    print(product)
+
+    return product
