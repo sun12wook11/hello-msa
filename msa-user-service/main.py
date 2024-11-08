e858ad209a670b6c6317a0d4f0c377a883d92012
sun12wook11 <sun12wook11@gmail.com>
2024-11-08 13:38:30 +0900

    init

--- msa-user-service/main.py
+++ msa-user-service/main.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,10 +9,13 @@ from service.database import create_tables
 app = FastAPI()
 
 # CORS 설정
-origins = [
-    "http://localhost:3000", # 허용할 프론트엔드 도메인
-    "http://127.0.0.1:3000"
-]
+# origins = [
+#     "http://localhost:3000", # 허용할 프론트엔드 도메인
+#     "http://127.0.0.1:3000",
+#     "http://msa-frontend-svc:3000",
+# ]
+origins = os.getenv('CORS_ORIGINS', 'http://localhost:3000').split(',')
+print('CORS -> ', origins)
 app.include_router(user.router)
 
 app.add_middleware(
